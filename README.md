ea811f8563fd09b2fad67d65c71b161e84ca62f5
mochineko <mochinekoo0113@gmail.com>
2025-11-19 21:48:10 +0900

    [add] 起動できるように!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python_Discord_Bot
+* Python用のDiscordBotです!
---
+++ run.py
@@ -0,0 +1,32 @@
+import json
+import os
+import sys
+
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+filename = "data.json"
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+if not os.path.exists(filename) or os.path.getsize(filename) == 0:
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump({"bot_token": "" }, f, ensure_ascii=False, indent=4)
+        print(f"{filename} を新しく作成しました。")
+
+with open(filename, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+if not data["bot_token"]:
+    print("Tokenが設定されていません")
+    sys.exit(0)
+
+@client.event
+async def on_ready():
+    print('ログインしました')
+    await tree.sync()
+
+
+client.run(data["bot_token"])
